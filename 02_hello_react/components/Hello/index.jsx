2a7891fdb3c320ead65fdcb3bee1936eae342c2d
hyl <1628617939@qq.com>
2021-10-16 18:50:23 +0800

    版本新建

---
+++ 02_hello_react/components/Hello/index.jsx
@@ -0,0 +1,7 @@
+import React, {Component} from "react";
+import hello from './index.module.css'
+export default class Hello extends  Component{
+    render() {
+        return <h1 className={hello.hello}>hello world!!</h1>
+    }
+}
